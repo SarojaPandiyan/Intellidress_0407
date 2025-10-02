5cec638552ece027677b12fd1c9b9c2f9b1db7c8
saroja <sarojapu0407@gmail.com>
2025-10-02 18:06:28 +0530

    done10

--- frontend/src/pages/DressSuggestions.js
+++ frontend/src/pages/DressSuggestions.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchDresses } from '../redux/slices/dressSlice'
+import { fetchDress } from '../redux/slices/dressSlice'
 
 const DressSuggestions = () => {
   const dispatch = useDispatch()
